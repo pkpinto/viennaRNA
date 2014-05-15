5786d5aa0126b3d1abd83209cabe7666977b9755
Paulo Pinto <p.pinto@uni-muenster.de>
2014-05-15 18:15:07 +0200

    Removed seqdata class
    Major refactoring of the code to remove the need for a seqdata class where all
    return computations are added.
    
    Check __main__ of ViennaRNA.py for example of use.

--- viennaRNA.py
+++ viennaRNA.py
@@ -4,30 +4,21 @@
 import ctypes
 import os
 
-# typedef struct
-# {
-#     char *sequence;
-#     char *structure;
-#     float mfe;
-#     float gfe;
-# } seqdata;
-class seqdata(ctypes.Structure):
-    _fields_ = [('sequence', ctypes.c_char_p), ('structure', ctypes.c_char_p),
-                ('mfe',ctypes.c_float), ('gfe',ctypes.c_float)]
+# char* seq_fold(char*, float*)
+seq_fold = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'viennarna.dylib')).seq_fold
+seq_fold.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_float)]
+seq_fold.restype = ctypes.c_char_p
 
-# void sequencefold(seqdata *s)
-seqfold = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'viennarna.dylib')).sequencefold
-seqfold.argtypes = [ctypes.POINTER(seqdata)]
-seqfold.restype = None
-
-# void sequencepf(seqdata *s)
-seqpf = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'viennarna.dylib')).sequencepf
-seqpf.argtypes = [ctypes.POINTER(seqdata)]
-seqpf.restype = None
+# char* seq_pf_fold(char*, float*)
+seq_pf_fold = ctypes.CDLL(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'viennarna.dylib')).seq_pf_fold
+seq_pf_fold.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_float)]
+seq_pf_fold.restype = ctypes.c_char_p
 
 if __name__ == '__main__':
 
-    sd = seqdata(sequence='CGCAGGGAUACCCGCGCC')
-    seqfold(ctypes.byref(sd))
-    seqpf(ctypes.byref(sd))
-    print('%s\n%s\n%f\n%f\n' % (sd.sequence, sd.structure, sd.mfe, sd.gfe))
+    sequence='CGCAGGGAUACCCGCGCC'
+    mfe, gfe = ctypes.c_float(), ctypes.c_float()
+    structure = seq_fold(sequence, ctypes.byref(mfe))
+    print('%s\n%s' % (sequence, structure))
+    structure = seq_pf_fold(sequence, ctypes.byref(gfe))
+    print('%s\n%f, %f' % (structure, mfe.value, gfe.value))
