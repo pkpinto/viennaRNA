80e89c4a1e1901d7fa5b8bef56ccc748623e2e18
Paulo Pinto <p.pinto@uni-muenster.de>
2015-05-29 11:47:52 +0200

    Bugfix for __init__.py
    In __init__.py temperature getter and setter was still being imported
    even now that no such functions exist.

--- viennaRNA/__init__.py
+++ viennaRNA/__init__.py
@@ -3,13 +3,13 @@
 from __version__ import __version_info__, __version__
 
 from fold import seq_fold, seq_pf_fold, seq_subopt, seq_eval,\
-    get_T, set_T, str_inverse, str_pf_inverse
+    str_inverse, str_pf_inverse
 
 from util import str_base_pairs, seq_str_compatible
 
 # previous line also place fold and util inside viennaRNA
 del fold, util
 
-__all__ = ['seq_fold', 'seq_pf_fold', 'seq_subopt', 'seq_eval',
-           'get_T', 'set_T', 'str_inverse', 'str_pf_inverse',
+__all__ = ['seq_fold', 'seq_pf_fold', 'seq_subopt',
+           'seq_eval', 'str_inverse', 'str_pf_inverse',
            'str_base_pairs', 'seq_str_compatible']
