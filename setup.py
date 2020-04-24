8269a0534a322e157bf5db2461a36d3d8fcc8995
Paulo Kauscher Pinto <2229378+pkpinto@users.noreply.github.com>
2020-04-24 22:32:47 +0200

    Update folder structure and build with setuptools

--- setup.py
+++ setup.py
@@ -1,32 +1,64 @@
 #!/usr/bin/env python
-# coding: utf-8
 
+import os
 from sys import platform
-from distutils.core import setup, Extension
 
-version = "__version__ = '0.0.0'"
-with open('viennaRNA/__version__.py', 'r') as f:
-    version = f.read()
-exec version
+from setuptools import setup, find_namespace_packages, Extension
+from setuptools.command.build_ext import build_ext
+
+
+TEST_REQUIRES = [
+    # testing and coverage
+    'pytest<5.3', 'coverage', 'pytest-cov', 'pylint',
+]
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+# by default, the .so built includes the platform name,
+# e.g. viennarna.cpython-37m-darwin.so, that is undesirable
+# https://stackoverflow.com/a/60285245
+class _build_ext(build_ext):
+    def get_ext_filename(self, ext_name):
+        filename = super().get_ext_filename(ext_name)
+        return os.path.join(os.path.split(filename)[0], 'viennarna.so')
 
 if platform == 'darwin':
-    module = Extension('viennaRNA.viennarna', ['viennaRNA/viennarna.c'],
-                       include_dirs=['/usr/local/include'],
-                       library_dirs=['/usr/local/lib'],
-                       libraries=['RNA'])
+    module = Extension('viennaRNA.viennarna', sources=['src/viennaRNA/viennarna.c'],
+                       libraries=['RNA'],)
 elif platform == 'linux2':
-    module = Extension('viennaRNA.viennarna', ['viennaRNA/viennarna.c'],
+    module = Extension('viennaRNA.viennarna', sources=['src/viennaRNA/viennarna.c'],
                        libraries=['RNA'],
                        extra_compile_args=['-std=c99'],
-                       extra_link_args=['-fopenmp'])
+                       extra_link_args=['-fopenmp'],)
 else:
     raise OSError('OS not supported.')
 
-setup(name='viennaRNA',
-      version=__version__,
-      description='Wrapper for the Vienna RNA folding library',
-      author='Paulo Pinto',
-      author_email='pmsppinto@me.com',
-      url='https://github.com/pmsppinto/viennaRNA',
-      ext_modules=[module],
-      packages=['viennaRNA'],)
+setup(
+    name='viennaRNA',
+    version='4.0',
+    description='Wrapper for the Vienna RNA folding library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+    ],
+    url='https://github.com/pkpinto/viennaRNA',
+    author='Paulo Kauscher Pinto',
+    author_email='paulo.kauscher.pinto@icloud.com',
+    license='Apache License 2.0',
+    package_dir={'': 'src'},
+    ext_modules=[module],
+    cmdclass={'build_ext': _build_ext},
+    packages=find_namespace_packages(where='src'),
+    install_requires=[],
+    extras_require={
+        'test': TEST_REQUIRES,
+    },
+)
--- src/viennaRNA/__init__.py
+++ src/viennaRNA/__init__.py
@@ -1,11 +1,9 @@
 # coding: utf-8
 
-from __version__ import __version_info__, __version__
-
-from fold import seq_fold, seq_pf_fold, seq_subopt, seq_eval,\
+from .fold import seq_fold, seq_pf_fold, seq_subopt, seq_eval,\
     str_inverse, str_pf_inverse
 
-from util import str_base_pairs, seq_str_compatible
+from .util import str_base_pairs, seq_str_compatible
 
 # previous line also place fold and util inside viennaRNA
 del fold, util
